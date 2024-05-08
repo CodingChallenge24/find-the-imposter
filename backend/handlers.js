bf54dc7647b9f890e4a827b95182b39ffa334505
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2024-05-08 19:52:03 +0700

    reduce duplicate codes

--- backend/handlers.js
+++ backend/handlers.js
@@ -12,38 +12,41 @@ function getNumImposters(arr) {
 
 function getResult(query) {
   const parts = query.split(" ");
+  let result;
   switch (parts[0]) {
     case TYPE.QUESTION:
-      return handleQues(parts.slice(1));
+      result = handleQues(parts.slice(1));
+      break;
     case TYPE.ANSWER:
-      return handleAns(parts.slice(1));
+      result = handleAns(parts.slice(1));
+      break;
     case TYPE.SOLUTION:
-      return { answer: "OK", solution: { n, k, imposIds } };
+      result = { answer: "OK", solution: { n, k, imposIds } };
+      break;
     default:
-      return { answer: "INVALID", message: "Invalid query" };
+      result = { message: "Invalid query" };
+      break;
   }
+  if (result.message) result = { answer: "INVALID", ...result };
+  return result;
 }
 
 function handleQues(arr) {
   const positions = arr.map((value) => parseInt(value));
   if (!val.allIntegers(positions))
     return {
-      answer: "INVALID",
       message: "Only integers are allowed",
     };
   if (!val.allInRange(positions, 1, n))
     return {
-      answer: "INVALID",
       message: `Numbers should be between 1 and ${n}`,
     };
   if (val.hasDuplicate(positions))
     return {
-      answer: "INVALID",
       message: "Duplicate numbers are not allowed",
     };
   if (arr.length !== 3)
     return {
-      answer: "INVALID",
       message: "Expected 3 numbers, but got " + arr.length,
     };
   return {
@@ -55,18 +58,15 @@ function handleAns(arr) {
   arr = arr.map((value) => parseInt(value));
   if (!val.allIntegers(arr))
     return {
-      answer: "INVALID",
       message: "Only integers are allowed",
     };
   const [out_k, ...positions] = arr;
   if (!val.allInRange(positions, 1, n))
     return {
-      answer: "INVALID",
       message: `Numbers should be between 1 and ${n}`,
     };
   if (val.hasDuplicate(positions))
     return {
-      answer: "INVALID",
       message: "Duplicate numbers are not allowed",
     };
   const posMatch = positions.filter((value) => imposIds.includes(value));
