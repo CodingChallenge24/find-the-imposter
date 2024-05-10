dd3d8be3374ca3ff511ddd0d0dde785455470e5a
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2024-05-10 14:12:42 +0700

    add "interact" event

--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const getResult = require('./handlers');
-const http = require('http');
-const { hasDuplicate } = require('./validator');
 const { Server } = require('socket.io');
-const cors = require('cors');
+const { createServer } = require('http');
 
-const PORT = process.env.PORT || 3000;
+const express = require('express');
+const { getResult } = require('./handlers');
 
-//// MIDDLEWARES
-const app = express().set('view engine', 'ejs');
-app.use(cors());
+const PORT = process.env.PORT || 4000;
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+const httpServer = createServer(express);
 
-// parse application/json
-app.use(bodyParser.json());
-
-//// ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.post('/ask', ({ body: { query, test } }, res) => {
-  const result = getResult(query);
-  if (test) res.render('index', { result: JSON.stringify(result) });
-  else res.json(result);
-});
-
-// Socket.io
-const server = http.createServer(app);
-const io = new Server(server, {
+const io = new Server(httpServer, {
   cors: {
     origin: '*',
   },
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+httpServer.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}.`);
 });
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+io.on('connect', (socket) => {
+  const username = `User ${socket.id.toString().slice(0, 6)}`;
+  console.log(`${username} is connected.`);
+
+  socket.on('interact', (query) => {
+    console.log(`${username} asks interactor with query "${query}"`);
+    socket.emit('interact', getResult(query));
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`${username} is disconnected.`);
+  });
 });
